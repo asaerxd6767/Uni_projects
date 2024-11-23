52d2278807f6e75f21ab77f86bf7d772a2a8e433
asaerxd6767 <90705181+asaerxd6767@users.noreply.github.com>
2024-11-23 19:48:00 +0200

    IT Encrypt Project.cpp

--- IT Project.cpp
+++ IT Project.cpp
@@ -8,30 +8,23 @@ int main()
     cout << "================" << char(187);
     cout << "\nCaesar Cipher:\tEncrypted Version.\n";
     cout << "================" << char(188);
-    string text, cypher;
+    string text, cipher;
     int key;
-    start:
     cout << "\n\nEnter lowercase text: ";
     getline(cin, text);
     cout << "\nEnter the key: ";
     cin >> key;
     for(int i = 0; i < text.length(); i++)
     {
-    if(text[i] == char(32)) //Add space into the cypher text.
-    {cypher += char(32);}
+    if(text[i] == char(32)) //Add space into the cipher text.
+    {cipher += char(32);}
 
     if (text[i] >= 97 && text[i] <= 122) //checks the lowercase input.
-    {cypher += ((text[i] - 97 + key) % 26) + 65;} //the calculation to convert it to capital + adding key.
+    {cipher += ((text[i] - 97 + key) % 26) + 65;} //the calculation to convert it to capital + adding key.
     }
-    cout << "\nThe Plain Text: " << text << endl;
-    cout << "The Cypher text: " << cypher << endl;
-    cout << "\nMade by: Eng/ Asser Mohammed Youssef\n";
-    cypher.clear();
-	char q;
-	cout << "\nAnother word?(y/n): ";
-	cin >> q;
-	if(q == 'y' || q == 'Y')
-    goto start;
+    cout << "\nThe Plain Text:  " << text << endl;
+    cout << "The Cipher text: " << cipher << endl;
+    cout << "\n\n================================\nMade by: Asser Mohammed Youssef.\n================================\n";
 return 0;
 }
 /*
@@ -41,14 +34,14 @@ How the project work:
 - If the letter is out of the alphabet table, the system will take the modulas of the ASCII code and will remove 26 which is alphabet number.
 
 Versions:
-1. v1.0 (13/11/24) - The user can enter only 4 characters and convert it to lowercase cypher character.
-2. v2.0 (15/11/24)- the user can enter a word and convert it to lowercase cypher word.
+1. v1.0 (13/11/24) - The user can enter only 4 characters and convert it to lowercase cipher character.
+2. v2.0 (15/11/24)- the user can enter a word and convert it to lowercase cipher word.
 4. v2.5 (19/11/24)- the user can enter many words but the user will enter "/" each time.
 5. v3.0 (23/11/24) - The final version.
 
 problems I faced:
 version 1: The code can use 4 characters. (fixed when added string and merged it the loop statement by taking the length)
-version 2: The cypher word was lowercased. (fixed when used ASCII code of capital alphabets)
+version 2: The cipher word was lowercased. (fixed when used ASCII code of capital alphabets)
 version 2.5: The system couldn't read the spaces. (fixed when added "getline" instead of "cin")
 made by: Asser Mohammed Youssef.
 */

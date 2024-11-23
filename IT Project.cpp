890547678a9ac556ee287c2c795b6fb0b524261f
asaerxd6767 <90705181+asaerxd6767@users.noreply.github.com>
2024-11-23 19:14:36 +0200

    IT Encrypt Project.cpp

--- IT Project.cpp
+++ IT Project.cpp
@@ -1,4 +1,4 @@
-//Caesar cipher.
+//Caesar cipher: Encrypt version.
 //version#2.99
 #include <iostream>
 #include <string>
@@ -8,27 +8,27 @@ int main()
     cout << "================" << char(187);
     cout << "\nCaesar Cipher:\tEncrypted Version.\n";
     cout << "================" << char(188);
-    string text, cipher;
+    string text, cypher;
     int key;
     start:
-    cout << "\n\nEnter lowercase text (Using / instead of space): ";
-    cin >> text;
+    cout << "\n\nEnter lowercase text: ";
+    getline(cin, text);
     cout << "\nEnter the key: ";
     cin >> key;
     for(int i = 0; i < text.length(); i++)
     {
-    if(text[i] == 47) //Converts the slash "/" into space.
-    {cipher += char(32); continue;}
+    if(text[i] == char(32)) //Add space into the cypher text.
+    {cypher += char(32);}
 
-    if (text[i] >= 97 && text[i] <= 122)
-    {cipher += ((text[i] - 97 + key) % 26) + 65;} 
+    if (text[i] >= 97 && text[i] <= 122) //checks the lowercase input.
+    {cypher += ((text[i] - 97 + key) % 26) + 65;} //the calculation to convert it to capital + adding key.
     }
     cout << "\nThe Plain Text: " << text << endl;
-    cout << "The Cipher text: " << cipher << endl;
-    cout << "\n Made by: Eng/ Asser Mohammed Youssef";
-    cipher.clear();
+    cout << "The Cypher text: " << cypher << endl;
+    cout << "\nMade by: Eng/ Asser Mohammed Youssef\n";
+    cypher.clear();
 	char q;
-	cout << endl << "Another word?(y/n): ";
+	cout << "\nAnother word?(y/n): ";
 	cin >> q;
 	if(q == 'y' || q == 'Y')
     goto start;
@@ -36,13 +36,19 @@ return 0;
 }
 /*
 How the project work:
-- The user will enter a word and the system will read how many letters the user wrote, this will determine when the loop will stop.
+- The user will enter the text and the system will read how many letters the user wrote, this will determine when the loop will stop.
 - Using the ASCII code, the system will convert the letters to ASCII code to add the key number, and convert them back to letters.
 - If the letter is out of the alphabet table, the system will take the modulas of the ASCII code and will remove 26 which is alphabet number.
 
-Problems to be fixed:
-DONE v2.5- the cypher text is lowercase. (TBF - read the ASCII code again and convert the text to capital alphabet numbers)
-TEMP DONE v2.99 the user can enter 1 word each time. (IDK how to fix it)
+Versions:
+1. v1.0 (13/11/24) - The user can enter only 4 characters and convert it to lowercase cypher character.
+2. v2.0 (15/11/24)- the user can enter a word and convert it to lowercase cypher word.
+4. v2.5 (19/11/24)- the user can enter many words but the user will enter "/" each time.
+5. v3.0 (23/11/24) - The final version.
 
+problems I faced:
+version 1: The code can use 4 characters. (fixed when added string and merged it the loop statement by taking the length)
+version 2: The cypher word was lowercased. (fixed when used ASCII code of capital alphabets)
+version 2.5: The system couldn't read the spaces. (fixed when added "getline" instead of "cin")
 made by: Asser Mohammed Youssef.
 */
